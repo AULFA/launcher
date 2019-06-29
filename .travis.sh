c652382347c8055487a36167255736945ac90a77
Mark Raynsford <code@io7m.com>
2019-06-29 11:30:31 +0000

    Add launcher password

--- .travis.sh
+++ .travis.sh
@@ -6,6 +6,13 @@ then
   exit 1
 fi
 
+if [ -z "${LFA_LAUNCHER_PASSWORD}" ]
+then
+  echo "LFA_LAUNCHER_PASSWORD not set"
+  exit 1
+fi
+
+
 mkdir -p "${HOME}/.ssh" || exit 1
 echo "${LFA_BUILDS_SSH_KEY}" | base64 -d > "${HOME}/.ssh/id_ed25519" || exit 1
 chmod 700 "${HOME}/.ssh" || exit 1
@@ -15,6 +22,11 @@ chmod 600 "${HOME}/.ssh/id_ed25519" || exit 1
 [builds.lfa.one]:1022 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/vroEIxH46lW/xg+CmCDwO7FHN24oP+ad4T/OtB/D2
 EOF
 ) >> "$HOME/.ssh/known_hosts" || exit 1
+
+(cat <<EOF
+password = ${LFA_LAUNCHER_PASSWORD}
+EOF
+) >> ./au.org.libraryforall.launcher.app/password.properties || exit 1
 
 (cat <<EOF
 
